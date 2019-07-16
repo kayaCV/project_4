5fc34e7c7d33cd3b76c296dbf61efa2f0e60e5f7
Carla <carlavdebarros@gmail.com>
2019-07-16 16:57:08 -0400

    Game{}; getRandomPhrase; Phrase()
    created Game {}, constructor, and getRandomPhrase();
    created Phrase{}, constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,47 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0
+        this.phrases = [
+            {phrase: 'it could be worse'}, 
+            {phrase: 'it takes two to tango'}, 
+            {phrase: 'your guess is as good as mine'}, 
+            {phrase: 'actions speak louder than words'}, 
+            {phrase: 'Back to the drawing board'}];
+        this.activePhrase = null;
+    }
+
+    // startGame() {
+
+
+    // }
+
+    getRandomPhrase() {  // Selects and return a random phrase from phrases property
+        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    }
+
+    // handleInteraction() {
+
+
+    // }
+
+    // removeLife() {
+
+
+    // }
+
+    // checkForWin() {
+
+
+    // }
+
+    // gameOver() {
+
+
+    // }
+ }
+
+ // 'it could be worse', 'it takes two to tango', 'your guess is as good as mine', 'actions speak louder than words', 'Back to the drawing board'
--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+// const phrase = new Phrase('Your guess is as good as mine');       // REMOVE
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+// const game = new Game();
+// game.phrases.forEach((phrase, index) => {
+// console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);    // REMOVE
+// });
+
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);      // REMOVE
+//     };                                                    // TEST FOR RANDOM PHRASES
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+
+
+const game = new Game();   //TEST FOR addPhraseToDisplay   REMOVE
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
